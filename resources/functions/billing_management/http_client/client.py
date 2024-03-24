46b1b525b57d335d1b37bfa7d7e6cca468f3e588
Derric Gilling <derric@moesif.com>
2024-03-23 19:37:09 -0700

    Add: Firehose for Ingestion, Zuora, Chargebee, and Custom Support
    1. Integration and Testing with Stripe, Zuora, Chargebee, and Custom Invoicing
    2. Added MoesifFirehoseConstruct to MoesifBilling
    3. Support to override collector base URL and event schema
    4. Add README.md

--- resources/functions/billing_management/http_client/client.py
+++ resources/functions/billing_management/http_client/client.py
@@ -8,6 +8,8 @@ moesif_management_api_key = os.environ['MOESIF_MANAGEMENT_API_KEY'].replace('Bea
 moesif_management_base_url = os.environ['MOESIF_MANAGEMENT_BASE_URL']
 billing_provider_slug = os.environ['BILLING_PROVIDER_SLUG']
 billing_provider_secret_key = os.environ['BILLING_PROVIDER_SECRET_KEY']
+billing_provider_client_id = os.environ['BILLING_PROVIDER_CLIENT_ID']
+billing_provider_base_url = os.environ['BILLING_PROVIDER_BASE_URL']
 
 def request_moesif_resource(method, resource, payload):
     headers = {
@@ -26,11 +28,40 @@ def request_stripe_resource(method, resource, payload):
     path = f'/v1/{resource}'
     return request('https://api.stripe.com', method, path, headers, payload, urlencode)
 
-def request(base_url, method, path, headers, payload, serialize):
+def request_chargebee_resource(method, resource, payload):
+    basic_auth = base64.b64encode(f'{billing_provider_secret_key}:'.encode('utf-8')).decode('utf-8')
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': f'Basic {basic_auth}'
+    }
+    path = f'/api/v2/{resource}'
+    return request(billing_provider_base_url, method, path, headers, payload, urlencode)
+
+def request_zuora_resource(method, resource, payload):
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = {
+        'grant_type': "client_credentials",
+        'client_id': billing_provider_client_id,
+        'client_secret': billing_provider_secret_key
+    }
+    auth_response = request(billing_provider_base_url, 'POST', '/oauth/token', headers, data, urlencode, False)
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {auth_response['access_token']}'
+    }
+    path = f'/v2/{resource}'
+    return request(billing_provider_base_url, method, path, headers, payload, json.dumps)
+
+def request(base_url, method, path, headers, payload, serialize, log_payload = True):
     conn = http.client.HTTPSConnection(base_url.replace('https://', ''), 443)
 
     print(f'Making request to {method} {base_url}{path}')
     payload_str = serialize(payload) if payload else None
+
+    if (log_payload):
+        print(payload_str)
 
     # Retry HTTP requests up to 3 times
     retry = 3
@@ -45,7 +76,6 @@ def request(base_url, method, path, headers, payload, serialize):
             return json.loads(response_body)
         except Exception as e:
             print(e)
-            print(payload) 
             if not retry:
                 raise e
             print(f'Retrying... Attempt {3 - retry}')
--- resources/functions/billing_management/services/subscription_service.py
+++ resources/functions/billing_management/services/subscription_service.py
@@ -1,6 +1,8 @@
 from http_client.client import *
+from datetime import datetime, timedelta
 
 billing_provider_slug = os.environ['BILLING_PROVIDER_SLUG']
+default_plan_id = os.environ['DEFAULT_PLAN_ID'] # Default plan to be used when creating new subscriptions
 default_price_id = os.environ['DEFAULT_PRICE_ID'] # Default price to be used when creating new subscriptions
 
 def create_subscription(detail):
@@ -11,7 +13,15 @@ def cancel_subscription(subscription_id):
     print(f'Request received to cancel subscription {subscription_id}')
 
     if (billing_provider_slug == 'stripe'):
-        request_stripe_resource('DELETE', f'subscriptions/{subscription_id}', None)
+        return request_stripe_resource('DELETE', f'subscriptions/{subscription_id}', None)
+    if (billing_provider_slug == 'chargebee'):
+        return request_chargebee_resource('POST', f'subscriptions/{subscription_id}/cancel_for_items', None)
+    if (billing_provider_slug == 'zuora'):
+        return request_zuora_resource('POST', f'subscriptions/{subscription_id}/cancel', {
+            'cancel_date': datetime.utcnow().strftime('%Y-%m-%d')
+        })
+    if (billing_provider_slug == 'custom'):
+        return request_moesif_resource('POST', f'subscriptions/{subscription_id}', None)
 
 def create_provider_resources(detail):
     # Not required to create a subscription in Moesif as provider will push this directly to Moesif
@@ -27,4 +37,57 @@ def create_provider_resources(detail):
             'items[0][price]': default_price_id
         }
         return request_stripe_resource('POST', 'subscriptions', new_subscription)
-    
+    elif (billing_provider_slug == 'chargebee'):
+        new_customer = {
+            'id': detail['tenantId'],
+            'email': f'{detail['tenantId']}@example.com', # FIXME Need Email for Billing
+            'meta_data[company_id]':  detail['tenantId']
+        }
+        customer = request_chargebee_resource('POST', 'customers', new_customer)
+        print(json.dumps(customer))
+        new_subscription = {
+            'customer': detail['tenantId'],
+            'subscription_items[item_price_id][0]': default_price_id
+        }
+        subscription_url = f'customers/{detail['tenantId']}/subscription_for_items'
+        return request_chargebee_resource('POST', subscription_url, new_subscription)
+    elif (billing_provider_slug == 'zuora'):
+        new_customer = {
+            'account_number': detail['tenantId'],
+            "name": detail['tenantId'],
+            'currency': 'USD', # FIXME should be set by tenant
+            'bill_to': {
+                'first_name': detail['tenantId'],
+                'last_name': detail['tenantId'],
+                'work_email': f'{detail['tenantId']}@example.com', # FIXME Need Email for Billing
+                'address': {
+                    'country': "USA", # FIXME should be set by tenant
+                    'state': "CA"
+                }
+            }
+        }
+        customer = request_zuora_resource('POST', 'accounts', new_customer)
+        print(json.dumps(customer))
+        new_subscription = {
+            'account_number': detail['tenantId'],
+            'subscription_plans': [{
+                'plan_id': default_plan_id,
+                'prices': [{
+                    'price_id': default_price_id
+                }]
+            }]
+        }
+        return request_zuora_resource('POST', 'subscriptions', new_subscription)
+    elif (billing_provider_slug == 'custom'):
+        current_time= datetime.utcnow()
+        new_subscription = {
+            'company_id': detail.get('tenantId'),
+            'subscription_id': detail.get('tenantId'),
+            'status': 'active',
+            'items': [{
+                'plan_id': default_plan_id
+            }],
+            'current_period_start': current_time.isoformat(),
+            'current_period_end': (current_time + timedelta(days=365)).isoformat(), # For now, assume one year subscription
+        }
+        return request_moesif_resource('POST', 'subscriptions', new_subscription)
